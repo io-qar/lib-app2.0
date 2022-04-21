703be6e1cbf94aabb13e2c0e93783ae2a98f5549
io-qar <koroold@gmail.com>
2022-04-21 23:45:47 +0300

    fix bat

--- start.bat
+++ start.bat
@@ -1,17 +1,17 @@
 @echo off
 cd backend-test
-if exist node_modules\ (
-	if exist ..\test-old-router\node_modules\ (
+if not exist node_modules\ (
+	if not exist ..\test-old-router\node_modules\ (
 		@REM как написать в одной сессии
-		
-		start cmd /k node server.js
+		start cmd /k npm i
 		cd ../test-old-router
-		npm start
+		npm i
 	)
-) else (
-	@REM как написать в одной сессии
-	
-	start cmd /k npm i
-	cd ../test-old-router
-	npm i
 )
+
+start cmd /k node server.js
+for %%I in (.) do set cd = %%~nxI
+if not %cd% == "test-old-router" (
+	cd ../test-old-router
+)
+npm start
